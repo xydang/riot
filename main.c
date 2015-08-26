1d5dc4979ccd614b01f557653bad8ce7691b533d
xydang <xydang@live.com>
2015-08-26 15:20:56 +0800

    fix bugs.

--- main.c
+++ main.c
@@ -16,34 +16,53 @@
 #define RESPONSE_ACK "{\"method\":\"response\",\"result\":{\"successful\":true,\"message\":\"ok\"}}&^!"
 #define SENSOR_DAT "{\"method\": \"upload\",\"data\":[{\"Name\":\"T1\",\"Value\":\"%2.1f\"},{\"Name\":\"H1\",\"Value\":\"%3d\"}]}&^!"
 
-struct timespec ts = {0,10000};
-char send_buf[1024];
+
+void msg_handle(char *msg_buf,int len)
+{
+	json_object *new_obj;
+
+	new_obj = json_tokener_parse(msg_buf);
+	json_object *obj= json_object_object_get(new_obj,"f");
+	char *method=(char *)json_object_get_string(obj);
+	if(strcmp(method,"getAllSensors")==0){
+		send(sockfd,RESPONSE_DEV,strlen(RESPONSE_DEV),0);
+	}else if(strcmp(method,"updateSensor")==0){
+		json_object *dev_obj= json_object_object_get(new_obj,"p1");
+		json_object *val_obj= json_object_object_get(new_obj,"p2");
+		if(strcmp(json_object_get_string(dev_obj),"sw0")==0){
+			if(strcmp(json_object_get_string(val_obj),"1")==0){
+				system("echo 1 > /sys/class/leds/user_led/brightness");
+				printf("sw0 on.\n");	
+			}else{
+				system("echo 0 > /sys/class/leds/user_led/brightness");
+				printf("sw0 off.\n");
+			}
+		}			
+		send(sockfd,RESPONSE_ACK,strlen(RESPONSE_ACK),0);
+		json_object_put(dev_obj);
+		json_object_put(val_obj);
+	}else if(strcmp(method,"writeSerial")==0){
+		json_object *val_obj= json_object_object_get(new_obj,"p1");
+		send(sockfd,RESPONSE_ACK,strlen(RESPONSE_ACK),0);
+		printf("write serial:%s\n",json_object_get_string(val_obj));
+		json_object_put(val_obj);
+	}else{
+		printf("%s\n", json_object_to_json_string(new_obj));
+		send(sockfd,RESPONSE_ACK,strlen(RESPONSE_ACK),0);
+	}
+	
+	json_object_put(new_obj);
+	json_object_put(obj);
+}
 
 void *task_recv(void *arg)
 {
 	int n=0;
-	json_object *new_obj;
-
+	struct timespec ts = {0,10000};
+	
 	while(1){
 		n=recv(sockfd,tcp_buf,1024,0);
-		new_obj = json_tokener_parse(tcp_buf);
-		json_object *obj= json_object_object_get(new_obj,"f");
-		if(strcmp(json_object_get_string(obj),"getAllSensors")==0){
-			send(sockfd,RESPONSE_DEV,strlen(RESPONSE_DEV),0);
-		}else if(strcmp(json_object_get_string(obj),"updateSensor")==0){
-			json_object *dev_obj= json_object_object_get(new_obj,"p1");
-			json_object *val_obj= json_object_object_get(new_obj,"p2");
-			if(strcmp(json_object_get_string(dev_obj),"sw0")==0){
-				if(strcmp(json_object_get_string(val_obj),"1")==0){
-					system("echo 1 > /sys/class/leds/user_led/brightness");
-					printf("sw0 on.\n");	
-				}else{
-					system("echo 0 > /sys/class/leds/user_led/brightness");
-					printf("sw0 off.\n");
-				}
-			}			
-			send(sockfd,RESPONSE_ACK,strlen(RESPONSE_ACK),0);
-		}
+		msg_handle(tcp_buf,n);
 		memset(tcp_buf,0,n);
 		n=0;
 		nanosleep(&ts, NULL);
@@ -53,6 +72,7 @@ void *task_recv(void *arg)
 void *task_send(void *arg)
 {
 	int n=0,k=0;
+	char send_buf[1024];
 
 	while(1){
 		n++;
@@ -87,11 +107,6 @@ int main(int argc,char *argv[])
 
 	return 0;
 }
-
-
-
-
-
 
 
 
